4393ec4d00bcb695f1f52926b1268f253d1c822d
Ludde86 <bslbjorn@gmail.com>
2020-06-05 09:58:47 +0200

    install validator

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,41 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('client side app.js is running');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person__ = __webpack_require__(2);
+// import './utils';
+// import subtract, { square, add } from './utils';
+
+// console.log('client side app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(10, 5));
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person__["c" /* isAdult */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person__["a" /* canDrink */](20));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person__["b" /* default */](65));
+
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isSenior; });
+const isAdult = (age) => age >= 18;
+
+const canDrink = (age) => age >= 21;
+
+const isSenior = (age) => age >= 65;
+
+
 
 
 /***/ })
